0a15e77d875a921cd22692b797cfcd6a9e1cd2c8
Cazolion <redhead.terror@gmail.com>
2022-10-05 13:42:38 +1100

    Add basic functionality

--- script.js
+++ script.js
@@ -37,81 +37,173 @@ const display = document.querySelector('.display');
 
 const one = document.getElementById("one");
 one.addEventListener('click', () => {
-    display.textContent += "1";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "1"
+    }
+    else {
+        display.textContent += "1";
+    }
 })
 
 const two = document.getElementById("two");
 two.addEventListener('click', () => {
-    display.textContent += "2";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "2"
+    }
+    else {
+        display.textContent += "2";
+    }
 })
 
 const three = document.getElementById("three");
 three.addEventListener('click', () => {
-    display.textContent += "3";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "3"
+    }
+    else {
+        display.textContent += "3";
+    }
 })
 
 const four = document.getElementById("four");
 four.addEventListener('click', () => {
-    display.textContent += "4";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "4"
+    }
+    else {
+        display.textContent += "4";
+    }
 })
 
 const five = document.getElementById("five");
 five.addEventListener('click', () => {
-    display.textContent += "5";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "5"
+    }
+    else {
+        display.textContent += "5";
+    }
 })
 
 const six = document.getElementById("six");
 six.addEventListener('click', () => {
-    display.textContent += "6";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "6"
+    }
+    else {
+        display.textContent += "6";
+    }
 })
 
 const seven = document.getElementById("seven");
 seven.addEventListener('click', () => {
-    display.textContent += "7";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "7"
+    }
+    else {
+        display.textContent += "7";
+    }
 })
 
 const eight = document.getElementById("eight");
 eight.addEventListener('click', () => {
-    display.textContent += "8";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "8"
+    }
+    else {
+        display.textContent += "8";
+    }
 })
 
 const nine = document.getElementById("nine");
 nine.addEventListener('click', () => {
-    display.textContent += "9";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "9"
+    }
+    else {
+        display.textContent += "9";
+    }
 })
 
 const zero = document.getElementById("zero");
 zero.addEventListener('click', () => {
-    display.textContent += "0";
+    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"){
+        display.textContent = "0"
+    }
+    else {
+        display.textContent += "0";
+    }
 })
 
-// DECIMAL DOT NEEDS TO ONLY BE ALLOWED ONCE BETWEEN OPERATORS
-// const dot = document.getElementById("dot");
-// dot.addEventListener('click', () => {
-//     display.textContent += ".";
-// })
+const dot = document.getElementById("dot");
+dot.addEventListener('click', () => {
+    display.textContent += ".";
+    document.getElementById("dot").disabled = true;
+})
 
 const plus = document.getElementById("plus");
 plus.addEventListener('click', () => {
-    display.textContent += "+";
+    a = parseFloat(display.textContent);
+    operator = "+";
+    display.textContent = "+";
+    document.getElementById("dot").disabled = false;
 })
 
 const minus = document.getElementById("minus");
 minus.addEventListener('click', () => {
-    display.textContent += "-";
+    a = parseFloat(display.textContent);
+    operator = "-";
+    display.textContent = "-";
+    document.getElementById("dot").disabled = false;
 })
 
 const multiplying = document.getElementById("multiply");
 multiplying.addEventListener('click', () => {
-    display.textContent += "*";
+    a = parseFloat(display.textContent);
+    operator = "*";
+    display.textContent = "*";
+    document.getElementById("dot").disabled = false;
 })
 
 const dividing = document.getElementById("divide");
 dividing.addEventListener('click', () => {
-    display.textContent += "/";
+    a = parseFloat(display.textContent);
+    operator = "/";
+    display.textContent = "/";
+    document.getElementById("dot").disabled = false;
 })
 
-const clear = document.getElementById("clear");
-clear.addEventListener('click', () => {
-    display.textContent = "";
+const equal = document.getElementById("equal");
+equal.addEventListener('click', () => {
+    b = parseFloat(display.textContent)
+    display.textContent = operate(a, operator, b);
+    document.getElementById("dot").disabled = false;
 })
+
+const allclear = document.getElementById("allclear");
+allclear.addEventListener('click', () => {
+    a = 0;
+    b = 0;
+    operator = "";
+    display.textContent = "";
+    document.getElementById("dot").disabled = false;
+})
+
+// const clearentry = document.getElementById("clearentry");
+// clearentry.addEventListener('click', () => {
+//     a = 0;
+//     display.textContent = "";
+//     document.getElementById("dot").disabled = false;
+// })
+
+let a = 0;
+let b = 0;
+let operator = "";
+
+
+// Bugs to fix
+// Clear entry doesn't work 
+// Clicking an operator first breaks calculator
+// NaN 
+// Keyboard feature
+// Chaining operators doesn't work
