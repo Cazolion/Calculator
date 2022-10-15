7e5707ff36802edbbeddfdd2aae263f72854c86c
Cazolion <redhead.terror@gmail.com>
2022-10-15 20:53:03 +1100

    Refactor all the things

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const equal = document.getElementById("equal");
+const dot = document.getElementById("dot");
+const allclear = document.getElementById("allclear");
+const clearentry = document.getElementById("clearentry");
+
+function show() {
+    let turtle = document.createElement("img")
+    display.appendChild(turtle);
+    turtle.setAttribute("width", "100")
+    turtle.setAttribute("src", './images/enguarde.png')
+    turtle.setAttribute("id", "turtle");
+    turtle.style.display = "inline-block";
+    setTimeout(
+        function hide() {
+        turtle.style.display = 'none'
+    }, 5000)
+}
+
 function add(a, b) {
     return a + b;
 };
@@ -33,160 +51,54 @@ function operate(a, operator, b) {
 };
 
 const display = document.querySelector('.display');
+const numerical = document.querySelectorAll('.numerical');
+const operation = document.querySelectorAll('.operation');
 
-const one = document.getElementById("one");
-one.addEventListener('click', () => {
+function getNumber(input) {
     if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/"|| display.textContent === "ERROR"){
-        display.textContent = "1"
+        display.textContent = input;
     }
     else {
-        display.textContent += "1";
+        display.textContent += input;
     }
+}
+
+function getOperator(inputOperator) {
+    a = parseFloat(display.textContent);
+    operator = inputOperator;
+    display.textContent = operator;
+    document.getElementById("dot").disabled = false;
+}
+
+numerical.forEach(numero => {
+    numero.addEventListener('click', () => {
+        getNumber(numero.textContent)
+    })
 })
 
-const two = document.getElementById("two");
-two.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "2"
-    }
-    else {
-        display.textContent += "2";
-    }
+operation.forEach(sign => {
+    sign.addEventListener('click', () => {
+        getOperator(sign.textContent)
+    })
 })
 
-const three = document.getElementById("three");
-three.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "3"
-    }
-    else {
-        display.textContent += "3";
-    }
-})
-
-const four = document.getElementById("four");
-four.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "4"
-    }
-    else {
-        display.textContent += "4";
-    }
-})
-
-const five = document.getElementById("five");
-five.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "5"
-    }
-    else {
-        display.textContent += "5";
-    }
-})
-
-const six = document.getElementById("six");
-six.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "6"
-    }
-    else {
-        display.textContent += "6";
-    }
-})
-
-const seven = document.getElementById("seven");
-seven.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "7"
-    }
-    else {
-        display.textContent += "7";
-    }
-})
-
-const eight = document.getElementById("eight");
-eight.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "8"
-    }
-    else {
-        display.textContent += "8";
-    }
-})
-
-const nine = document.getElementById("nine");
-nine.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "9"
-    }
-    else {
-        display.textContent += "9";
-    }
-})
-
-const zero = document.getElementById("zero");
-zero.addEventListener('click', () => {
-    if (display.textContent === "+" || display.textContent === "-" || display.textContent === "*" || display.textContent === "/" || display.textContent === "ERROR"){
-        display.textContent = "0"
-    }
-    else {
-        display.textContent += "0";
-    }
-})
-
-const dot = document.getElementById("dot");
 dot.addEventListener('click', () => {
     display.textContent += ".";
     document.getElementById("dot").disabled = true;
 })
 
-const plus = document.getElementById("plus");
-plus.addEventListener('click', () => {
-    a = parseFloat(display.textContent);
-    operator = "+";
-    display.textContent = "+";
-    document.getElementById("dot").disabled = false;
-})
-
-const minus = document.getElementById("minus");
-minus.addEventListener('click', () => {
-    a = parseFloat(display.textContent);
-    operator = "-";
-    display.textContent = "-";
-    document.getElementById("dot").disabled = false;
-})
-
-const multiplying = document.getElementById("multiply");
-multiplying.addEventListener('click', () => {
-    a = parseFloat(display.textContent);
-    operator = "*";
-    display.textContent = "*";
-    document.getElementById("dot").disabled = false;
-})
-
-const dividing = document.getElementById("divide");
-dividing.addEventListener('click', () => {
-    a = parseFloat(display.textContent);
-    operator = "/";
-    display.textContent = "/";
-    document.getElementById("dot").disabled = false;
-})
-
-const equal = document.getElementById("equal");
 equal.addEventListener('click', () => {
     b = parseFloat(display.textContent)
     display.textContent = operate(a, operator, b);
     document.getElementById("dot").disabled = false;
 })
 
-const allclear = document.getElementById("allclear");
 allclear.addEventListener('click', () => {
     operator = "";
     display.textContent = "";
     document.getElementById("dot").disabled = false;
 })
 
-const clearentry = document.getElementById("clearentry");
 clearentry.addEventListener('click', () => {
     display.textContent = "";
     document.getElementById("dot").disabled = false;
@@ -204,16 +116,4 @@ let operator = "";
 // Chaining operators doesn't work
 // No rounding up 
 
-function show() {
-    let turtle = document.createElement("img")
-    display.appendChild(turtle);
-    turtle.setAttribute("width", "100")
-    turtle.setAttribute("src", './images/enguarde.png')
-    turtle.setAttribute("id", "turtle");
-    document.getElementById('turtle').style.display = "inline-block";
-    setTimeout(
-        function hide() {
-        document.getElementById('turtle').style.display = 'none'
-    }, 5000)
-}
 
